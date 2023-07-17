2e262536a665670c5dbde4c36019a3cef052f5e1
Angelica Marín <marinange22@gmail.com>
2023-07-16 23:45:49 -0600

    Cambios de diseño en Tienda Virtuall
    Se cambiarón colores, se agregó el carrito arriba, el carrito es funcional, se arreglo agregar productos iguales, etc.

--- views/assets/js/TiendaVirtual.js
+++ views/assets/js/TiendaVirtual.js
@@ -1,35 +1,67 @@
+// JavaScript para el carrito de compras
 let cartItems = [];
 let cartTotal = 0;
 
-function addToCart(productName, price) {
-  cartItems.push({ name: productName, price: price });
-  cartTotal += price;
-  
+function addToCart(itemName, itemPrice) {
+  let existingItem = cartItems.find(item => item.name === itemName);
+
+  if (existingItem) {
+    // El producto ya existe en el carrito, se suma el precio y la cantidad
+    existingItem.price += itemPrice;
+    existingItem.quantity++;
+  } else {
+    // El producto no existe en el carrito, se agrega como un nuevo elemento
+    const newItem = {
+      name: itemName,
+      price: itemPrice,
+      quantity: 1
+    };
+    cartItems.push(newItem);
+  }
+
+  cartTotal += itemPrice;
   updateCart();
+
 }
 
-function updateCart() {
-  const cartItemsElement = document.getElementById('cart-items');
-  const cartTotalElement = document.getElementById('cart-total');
 
-  cartItemsElement.innerHTML = '';
+function updateCart() {
+  const cartItemsList = document.getElementById('cart-items');
+  const cartTotalElement = document.getElementById('cart-total');
+  const cartCountElement = document.getElementById('cart-count');
+
+
+  cartItemsList.innerHTML = '';
   cartItems.forEach(item => {
-    const li = document.createElement('li');
-    li.textContent = `${item.name} ${item.price}`;
-    cartItemsElement.appendChild(li);
+    const listItem = document.createElement('li');
+    listItem.textContent = `${item.name} (${item.quantity}) - &#8353;${item.price}`;
+    cartItemsList.appendChild(listItem);
   });
 
   cartTotalElement.textContent = cartTotal;
+  cartCountElement.textContent = cartItems.length;
 }
 
+function Abrir_Carrito() {
+  const carrito = document.getElementById('carrito');
+  carrito.style.display = 'block';
+}
+
+function Cerrar_Carrito() {
+  const carrito = document.getElementById('carrito');
+  carrito.style.display = 'none';
+}
+
+
+
 function Abrir_Nav() {
-    document.getElementById("main").style.marginLeft = "10%";
-    document.getElementById("miSidebar").style.width = "10%";
-    document.getElementById("miSidebar").style.display = "block";
-    document.getElementById("AbrirNav").style.display = 'none';
+  document.getElementById("main").style.marginLeft = "10%";
+  document.getElementById("miSidebar").style.width = "10%";
+  document.getElementById("miSidebar").style.display = "block";
+  document.getElementById("AbrirNav").style.display = 'none';
 }
 function Cerrar_Nav() {
-    document.getElementById("main").style.marginLeft = "0%";
-    document.getElementById("miSidebar").style.display = "none";
-    document.getElementById("AbrirNav").style.display = "inline-block";
+  document.getElementById("main").style.marginLeft = "0%";
+  document.getElementById("miSidebar").style.display = "none";
+  document.getElementById("AbrirNav").style.display = "inline-block";
 }
